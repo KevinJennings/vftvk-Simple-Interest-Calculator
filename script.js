b92dd1b7ba9d2d332aafff6aac5fc7c132148eb1
Kevin Jennings <kevin.jennings@gmail.com>
2021-08-08 19:21:21 -0500

    Updated script.js

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 function compute()
+{    
+    var principal = Number(document.getElementById("principal").value);
+
+    /* Make sure we have a valid number before proceeding further. 
+    Best to check now before trying to use invalid input in a calculation. */
+    if (principal <= 0) {
+        alert("Enter a positive number.");
+        return;
+    }
+
+    // To ensure type safety, I've added "Number()" where appropriate
+    var rate = Number(document.getElementById("rate").value);
+    var years = parseInt(document.getElementById("years").value);  
+    var interest = Number(principal * years * rate/100);  
+    var year = Number(new Date().getFullYear()+parseInt(years)); 
+
+    document.getElementById("result").innerHTML=
+    "\<br\>If you deposit \<span class='highlight'>"+principal+"\</span>,\<br\> at an interest rate of \<span class='highlight'>"+rate+
+    "%\</span>,\<br\>you will receive an amount of \<span class='highlight'>"+interest+"\</span>,\<br\>in the year \<span class='highlight'>"
+    +year+"\</span>\<br\>";
+
+    // All done! Set the focus of the cursor.
+    document.getElementById("principal").focus();
+};
+
+
+function updateRate() 
 {
-    p = document.getElementById("principal").value;
-    
-}
+    // Update the interest rate on the screen.    
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval+"%";    
+};
         
